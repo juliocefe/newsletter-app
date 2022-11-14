e7ca2b3f30e5c5a22d4b714d0b3f625dbec007df
juliocefe <juliocesarfelixhz95@gmail.com>
2022-11-14 16:57:13 -0700

    feature: config cors for local development

--- newsletterapp/settings.py
+++ newsletterapp/settings.py
@@ -30,18 +30,20 @@ DJANGO_APPS = [
 
 THIRDPARTY_APPS = [
     'rest_framework',
-    "rest_framework.authtoken",
+    'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 LOCAL_APPS = [
-    "newsletterapp.users.apps.UsersConfig",
-    "newsletterapp.newsletters.apps.NewslettersConfig",
+    'newsletterapp.users.apps.UsersConfig',
+    'newsletterapp.newsletters.apps.NewslettersConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRDPARTY_APPS + LOCAL_APPS
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -139,3 +141,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated'
     ]
 }
+
+# django cors
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
