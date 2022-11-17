3b0645486d1635c941130275ffd3808f63f12619
juliocefe <juliocesarfelixhz95@gmail.com>
2022-11-17 03:11:09 -0700

    logo to login

--- newsletter_spa/src/components/login/login.jsx
+++ newsletter_spa/src/components/login/login.jsx
@@ -2,13 +2,14 @@ import Button from "@mui/material/Button";
 import { Box } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import { useLogin } from "./useLogin";
+import Logo from '/src/assets/logo.png';
 
 function Input({ state, name, extraAtrs }) {
   return (
     <TextField
       {...state}
       id={name}
-      label={`${name} *`}
+      label={name}
       variant="outlined"
       sx={{ mb: 2 }}
       {...extraAtrs}
@@ -34,8 +35,11 @@ function Login() {
           flexDirection: "column",
         }}
       >
-        <Box component={"h1"} sx={{ fontSize: 30, textAlign: "center" }}>
-          Sign in
+        <Box sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+          <Box component={"img"} src={Logo} sx={{width: 80, height: 80}}/>
+          <Box component={"h1"} sx={{ fontSize: 30, textAlign: "center" }}>
+            Sign in
+          </Box>
         </Box>
         <Input state={username} name="username" />
         <Input
